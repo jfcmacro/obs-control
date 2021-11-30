a767260f679e46bbfdba0db15e00b12b6b5b91e8
Juan Francisco Cardona Mc'Cormick <jfcmacro@gmail.com>
2021-11-29 19:57:29 -0500

    The first version working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # obs-control
 This application control two instances in the same machine of an obs application
+
+## Nothing new added
---
+++ obscontrol.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdlib>
+#include "obscontrol.h"
+
+int
+main(int argc, char *argv[]) {
+
+  return EXIT_SUCCESS;
+}
